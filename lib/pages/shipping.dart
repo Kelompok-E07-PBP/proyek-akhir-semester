a400e773fce147999352ff4875626d4b660b51a7
Raffi Dary <rraffidh@gmail.com>
2024-12-02 18:12:35 +0700

    Changed left drawer to bottom navbar

---
+++ lib/pages/shipping.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ShippingPage extends StatelessWidget {
+  const ShippingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Shipping Page'),
+    );
+  }
+}
