e2a4eab2ba9dab9d7e07a1bb4d8b7d8bd6875a32
Mabacore101 <valentinozhuo2@gmail.com>
2024-12-09 15:48:44 +0700

    Selesai implementasi halaman login dan cicil pembuatan form.

---
+++ lib/pages/productentry_form.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ProductEntryFormPage extends StatefulWidget {
+  const ProductEntryFormPage({super.key});
+
+  @override
+  State<ProductEntryFormPage> createState() => _ProductEntryFormPageState();
+}
+
+class _ProductEntryFormPageState extends State<ProductEntryFormPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _namaProduk = "";
+  String _kategori = "";
+  int _harga = 0;
+  String _gambarProduk = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Form Tambah Produk',
+          ),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Colors.white,
+      ),
+      body: Form(
+        key:_formKey,
+        child: SingleChildScrollView(
+          child: Column()
+        ),
+      ),
+    );
+  }
+}
